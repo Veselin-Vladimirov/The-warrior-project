921c8ab42fce52b03f31a0db542a2485bc47abbf
DeyanFit <101415024+DeyanFit@users.noreply.github.com>
2024-03-28 20:19:38 +0200

    Delete app/ecsdeploy.sh

--- app/ecsdeploy.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-ecs-cli compose --project-name warrior  --file docker-compose.yaml \
---debug service up  \
---deployment-max-percent 200 --deployment-min-healthy-percent 100 \
---region eu-central-1 --ecs-profile warrior --cluster-config warrior
