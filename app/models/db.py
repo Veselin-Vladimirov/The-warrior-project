130f1dcb783e9e73be53ec1a0c83b26ab99a611a
DeyanFit <101415024+DeyanFit@users.noreply.github.com>
2024-04-01 20:10:25 +0300

    Virt RDS changes

--- app/models/db.py
+++ app/models/db.py
@@ -3,9 +3,10 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-# os.environ['SQLALCHEMY_DATABASE_URI']
-# 'postgresql://postgres:1234@localhost:5432/sensor_db'
-engine = create_engine('postgresql://postgres:1234@localhost:5432/sensor_db')
+
+database_uri = os.environ.get('SQLALCHEMY_DATABASE_URI', 'postgresql://postgres:1234@localhost:5432/sensor_db')
+
+engine = create_engine(database_uri)
 
 Base = declarative_base()
 Base.metadata.bind = engine
