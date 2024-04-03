44321f2843cf24cc1a2f825d505a1f29c5f3b934
deyanatanasov <deyan.s.atanasov.2019@elsys-bg.org>
2024-04-03 17:46:22 +0000

    changed urls

--- app/models/db.py
+++ app/models/db.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 
-database_uri = os.environ.get('SQLALCHEMY_DATABASE_URI', 'postgresql://postgres:1234@localhost:5432/sensor_db')
+database_uri = os.environ.get('SQLALCHEMY_DATABASE_URI', 'postgresql://warrior:1122334455@postgres-db1.ctyge4wgoiux.eu-central-1.rds.amazonaws.com:5432/postgresdb')
 
 engine = create_engine(database_uri)
 
