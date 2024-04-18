dc0015d464e29a6699e26040044a53f201391abc
Archer743 <61616845+Archer743@users.noreply.github.com>
2024-04-18 15:30:04 +0300

    [UPDATED] Flask endpoints + added more graphs

---
+++ app/app.py
@@ -0,0 +1,93 @@
+from flask import Flask, render_template, request, redirect, url_for, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+app = Flask(__name__, static_folder='static')
+
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:1234@localhost:5432/6amandura_db"
+
+db = SQLAlchemy(app)
+
+class Sensor(db.Model):
+    __tablename__ = 'records'
+    __table_args__ = { 'schema': 'public' }
+
+    id = db.Column(db.Integer, primary_key=True)
+    timestamp = db.Column(db.DateTime, nullable=False, default=datetime.now)
+
+    location = db.Column(db.Text, nullable=False)
+    air_temperature = db.Column(db.Float, nullable=False)
+    wind_speed = db.Column(db.Float, nullable=False)
+    humidity = db.Column(db.Float, nullable=False)
+    clearness = db.Column(db.Float, nullable=False)
+
+    water_temperature = db.Column(db.Float, nullable=False)
+    depth = db.Column(db.Float, nullable=False)
+    pressure = db.Column(db.Float, nullable=False)
+
+with app.app_context():
+    db.create_all()
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/graphs/<location>')
+def graphs(location):
+    return render_template('graphs.html', location=location)
+
+
+@app.route('/locations/<location>', methods=['GET'])
+def get_records_data_by_location(location):
+    location_records = Sensor.query.filter_by(location=location).all()
+
+    if not location_records:
+        return jsonify({'error': 'Location could not be found!'}), 404
+
+    data = {
+        'timestamps': [record.timestamp for record in location_records],
+        'air_temperatures': [record.air_temperature for record in location_records],
+        'wind_speeds': [record.wind_speed for record in location_records],
+        'humidities': [record.humidity for record in location_records],
+        'clearness': [record.clearness for record in location_records],
+        'water_temperatures': [record.water_temperature for record in location_records],
+        'depths': [record.depth for record in location_records],
+        'pressures': [record.pressure for record in location_records]
+    }
+
+    return jsonify(data)
+
+@app.route('/records', methods=['POST'])
+def add_new_record():
+    data = request.json
+
+    location = data.get('location')
+    air_temperature = data.get('air_temperature')
+    wind_speed = data.get('wind_speed')
+    humidity = data.get('humidity')
+    clearness = data.get('clearness')
+    water_temperature = data.get('water_temperature')
+    depth = data.get('depth')
+    pressure = data.get('pressure')
+    
+    if None not in [location, air_temperature, wind_speed, humidity, water_temperature, depth, pressure]:
+        new_sensor = Sensor(location=location,
+                            air_temperature=air_temperature,
+                            wind_speed=wind_speed,
+                            humidity=humidity,
+                            clearness=clearness,
+                            water_temperature=water_temperature,
+                            depth=depth,
+                            pressure=pressure)
+        
+        db.session.add(new_sensor)
+        db.session.commit()
+
+        return jsonify({'message': 'Data received successfully'}), 200
+    else:
+        return jsonify({'error': 'One or more parameters missing'}), 400
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5564, debug=True)
--- app/static/js/sensor_graph.js
+++ app/static/js/sensor_graph.js
@@ -1,46 +1,74 @@
-fetch('/sensor-data')
-    .then(response => response.json())
-    .then(data => {
-        const { temperatures, humidities, wind_speeds, pressures } = data;
-        const ctx = document.getElementById('sensorChart').getContext('2d');
-        const sensorChart = new Chart(ctx, {
-            type: 'line',
-            data: {
-                labels: Array.from({ length: temperatures.length }, (_, i) => i + 1),
-                datasets: [
-                    {
-                        label: 'Temperature',
-                        data: temperatures,
-                        borderColor: 'rgb(255, 99, 132)',
-                        backgroundColor: 'rgba(255, 99, 132, 0.5)',
-                    },
-                    {
-                        label: 'Humidity',
-                        data: humidities,
-                        borderColor: 'rgb(54, 162, 235)',
-                        backgroundColor: 'rgba(54, 162, 235, 0.5)',
-                    },
-                    {
-                        label: 'Wind Speed',
-                        data: wind_speeds,
-                        borderColor: 'rgb(255, 206, 86)',
-                        backgroundColor: 'rgba(255, 206, 86, 0.5)',
-                    },
-                    {
-                        label: 'Pressure',
-                        data: pressures,
-                        borderColor: 'rgb(75, 192, 192)',
-                        backgroundColor: 'rgba(75, 192, 192, 0.5)',
-                    }
-                ]
+const addSensorCharts = async (location) => {
+    try {
+        const response = await fetch(`/locations/${location}`);
+
+        if (!response.ok) {
+            const errorMsg = (await response.json()).error;
+            throw new Error(errorMsg);
+        }
+
+        const data = await response.json();
+
+        const chartsData = [
+            ["Air Temperature", data.air_temperatures, data.timestamps, 'Temperature °C', 'rgba(255, 99, 132, 0.2)', 'rgba(255, 99, 132, 1)'],
+            ["Humidity", data.humidities, data.timestamps, 'Humidity (%)', 'rgba(255, 206, 86, 0.2)', 'rgba(255, 206, 86, 1)'],
+            ["Wind Speed", data.wind_speeds, data.timestamps, 'Wind Speed (m/s)', 'rgba(75, 192, 192, 0.2)', 'rgba(75, 192, 192, 1)'],
+            ["Clearness", data.clearness, data.timestamps, 'Clearness (%)', 'rgba(120, 120, 120, 0.2)', 'rgba(120, 120, 120, 1)'],
+            ["Water Temperature", data.water_temperatures, data.timestamps, 'Temperature °C', 'rgba(0, 255, 255, 0.2)', 'rgba(0, 255, 255, 1)'],
+            ["Depth", data.depths, data.timestamps, 'Depth (m)', 'rgba(0, 0, 255, 0.2)', 'rgba(0, 0, 255, 1)'],
+            ["Pressure", data.pressures, data.timestamps, 'Pressure (mb)', 'rgba(54, 162, 235, 0.2)', 'rgba(54, 162, 235, 1)']
+        ];
+
+        for (const [title, data, timestamps, yLabel, backgroundColor, borderColor] of chartsData) {
+            createDataChart(title, data, timestamps, yLabel, backgroundColor, borderColor);
+        }
+    } catch (error) {
+        const errorMessageElement = document.createElement('h3');
+        errorMessageElement.textContent = error.message;
+
+        const graphDivElement = document.getElementById('graph-div');
+        graphDivElement.appendChild(errorMessageElement);
+    }
+}
+
+const createDataChart = (title, data, timestamps, yLabel, backgroundColor, borderColor) => {
+    const sensorChartElement = document.createElement('canvas');
+    sensorChartElement.width = 100;
+    sensorChartElement.height = 50;
+
+    const ctx = sensorChartElement.getContext('2d');
+    
+    new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: timestamps,
+            datasets: [{
+                data: data,
+                backgroundColor: backgroundColor,
+                borderColor: borderColor,
+                borderWidth: 1
+            }]
+        },
+        options: {
+            plugins: {
+                title: { display: true, text: title,
+                         align: 'center', font: { size: 20 } },
+                legend: { display: false }
             },
-            options: {
-                scales: {
-                    y: {
-                        beginAtZero: true
-                    }
+            scales: {
+                y: {
+                    title: { display: true, text: yLabel,
+                             align: 'center', font: { size: 18 } }
+                },
+                x: {
+                    title: { display: true, text: 'Timestamp',
+                             align: 'center', font: { size: 18 } },
+                    ticks: { autoSkip: true, maxTicksLimit: 20, }
                 }
             }
-        });
-    })
-    .catch(error => console.error('Error fetching sensor data:', error));
+        }
+    });
+
+    const graphDivElement = document.getElementById('graph-div');
+    graphDivElement.appendChild(sensorChartElement);
+}
