8bb6eb6a3e7d94b06f0108e6f352dc8c3fa45e5e
Deyan <deyan.s.atanasov.2019@elsys-bg.org>
2024-04-18 16:09:41 +0300

    remove space

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ from datetime import datetime
 
 app = Flask(__name__, static_folder='static')
 
-app.config['SQLALCHEMY_DATABASE_URI'] = " postgresql://warrior:1122334455@postgres-db1.ctyge4wgoiux.eu-central-1.rds.amazonaws.com:5432/postgresdb"
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://warrior:1122334455@postgres-db1.ctyge4wgoiux.eu-central-1.rds.amazonaws.com:5432/postgresdb"
 
 db = SQLAlchemy(app)
 
